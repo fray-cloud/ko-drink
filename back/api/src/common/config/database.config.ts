a3384cf52db06ecb9273cd6f2a24ea19cc5be406
woohyunKim <whkim@kephas.kr>
2025-12-14 22:28:16 +0900

    🔧 환경변수명 변경

--- back/api/src/common/config/database.config.ts
+++ back/api/src/common/config/database.config.ts
@@ -2,11 +2,11 @@ import { registerAs } from '@nestjs/config';
 
 export default registerAs('database', () => ({
   type: 'postgres',
-  host: process.env.DB_HOST || 'localhost',
-  port: parseInt(process.env.DB_PORT || '5432', 10),
-  username: process.env.DB_USERNAME || 'postgres',
-  password: process.env.DB_PASSWORD || 'postgres',
-  database: process.env.DB_DATABASE || 'kodrink',
+  host: process.env.DATABASE_HOST || 'localhost',
+  port: parseInt(process.env.DATABASE_PORT || '5432', 10),
+  username: process.env.DATABASE_USER || 'postgres',
+  password: process.env.DATABASE_PASSWORD || 'postgres',
+  database: process.env.DATABASE_NAME || 'kodrink',
   entities: [__dirname + '/../../**/*.entity{.ts,.js}'],
   synchronize: process.env.NODE_ENV !== 'production',
   logging: process.env.NODE_ENV === 'development',
